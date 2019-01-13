42288782e794bd4781f2005175a0db8627a2cc72
Benyam Ephrem <ephremdsg@gmail.com>
2019-01-13 14:04:16 -0500

    merge sorted lists

--- Trees, Binary Trees, & Binary Search Trees/lowestCommonAncestorBinaryTree.java
+++ Trees, Binary Trees, & Binary Search Trees/lowestCommonAncestorBinaryTree.java
@@ -1,24 +1,11 @@
 /**
   @author Benyam Ephrem
 
+  Lowest Common Ancestor of a Binary Tree - LeetCode:
+  https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree
+
   This code passes all Leetcode test cases as of Jan. 1st 2019
-  Runtime: 14 ms, faster than 23.21% of Java online submissions* for
-  Lowest Common Ancestor of a Binary Tree.
-
-  This is the recursion approach and has the same runtime as the
-  accepted recursion approach on Leetcode's "solution" section.
-
-  Complexities:
-    # Let n be the total nodes in the binary tree.
-  Time:
-    - O(n) worst case if we have to touch all n nodes in our search
-  Space:
-    - Stack frames will take up space through the recursion
-    - O(h) best case if we have a balanced binary tree
-    - O(n) worst case if we have a purely left or right skewed tree
-      and have to find the LCA of two nodes at the bottom of the skewed
-      tree (and technically in this worst case n IS h but we say n because
-      it makes more sense and is more accurate to what is happening)
+  Runtime: 13 ms, faster than 91.14% of Java online submissions for Lowest Common Ancestor of a Binary Tree.
 
   The video to explain this code is here: [a link will live here someday]
 */
@@ -35,13 +22,8 @@ public TreeNode search(TreeNode root, TreeNode p, TreeNode q) {
   TreeNode leftSearchResult = search(root.left, p, q);
   TreeNode rightSearchResult = search(root.right, p, q);
   
-  if (leftSearchResult == null) {
-      return rightSearchResult;
-  }
-  
-  if (rightSearchResult == null) {
-      return leftSearchResult;
-  }
+  if (leftSearchResult == null) return rightSearchResult;
+  if (rightSearchResult == null) return leftSearchResult;
   
   return root;
 }
