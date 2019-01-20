f63371edf6b262eb77eb89121b6fcf4f3f462916
Benyam Ephrem <ephremdsg@gmail.com>
2019-01-20 17:10:16 -0500

    mm

--- Arrays, Primitives, Strings/interconvertStringAndInt.java
+++ Arrays, Primitives, Strings/interconvertStringAndInt.java
@@ -11,51 +11,51 @@
 
 public static String intToString(int inputInt) {
 
-	/*
-		Take note if the integer is negative before
-		we operate on it and take it to 0 during
-		our truncation
-	*/
-	boolean isNegative = false;
+  /*
+    Take note if the integer is negative before
+    we operate on it and take it to 0 during
+    our truncation
+  */
+  boolean isNegative = false;
 
-	if (inputInt < 0) {
-		isNegative = true;
-	}
+  if (inputInt < 0) {
+    isNegative = true;
+  }
 
-	/*
-		"Eat" away the string character by character:
+  /*
+    "Eat" away the string character by character:
 
-		1.) Convert the ones place to a character
-		2.) Append it to the target string
-		3.) "Eat" away the ones place from the integer
-	*/
-	StringBuilder sb = new StringBuilder();
-	do {
+    1.) Convert the ones place to a character
+    2.) Append it to the target string
+    3.) "Eat" away the ones place from the integer
+  */
+  StringBuilder sb = new StringBuilder();
+  do {
 
-		int onesPlace = inputInt % 10;
-		char onesPlaceAsCharacter = (char) '0' + Math.abs(onesPlace);
+    int onesPlace = inputInt % 10;
+    char onesPlaceAsCharacter = (char) '0' + Math.abs(onesPlace);
 
-		sb.append(onesPlaceAsCharacter);
-		inputInt /= 10;
+    sb.append(onesPlaceAsCharacter);
+    inputInt /= 10;
 
-	} while (inputInt != 0);
+  } while (inputInt != 0);
 
-	if (isNegative) {
-		sb.append('-'); // Adds the negative sign to the end
-	}
+  if (isNegative) {
+    sb.append('-'); // Adds the negative sign to the end
+  }
 
-	/*
-		By this point if the input was -123 then we will
-		have "321-"
+  /*
+    By this point if the input was -123 then we will
+    have "321-"
 
-		Reversed we will get what we want, "-123"
-	*/
-	sb.reverse();
+    Reversed we will get what we want, "-123"
+  */
+  sb.reverse();
 
-	/*
-		Finally, convert the StringBuilder object to a string
-	*/
-	return sb.toString();
+  /*
+    Finally, convert the StringBuilder object to a string
+  */
+  return sb.toString();
 }
 
 
