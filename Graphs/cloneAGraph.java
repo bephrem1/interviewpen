7a6838bdc0de7c8b4df4615eef18dfbe52f900c2
Benyam Ephrem <ephremdsg@gmail.com>
2019-01-26 10:56:45 -0500

    COMMENTS :)

--- Graphs/cloneAGraph.java
+++ Graphs/cloneAGraph.java
@@ -17,30 +17,67 @@
 
 public UndirectedGraphNode cloneGraph(UndirectedGraphNode start) {
 
+  /*
+    If the start node is null then we cannot do any cloning
+  */
   if(start == null) {
     return null;
   }
 
+  /*
+    vertexMap: Map the original node reference to its clone
+    queue: Our queue for Breadth First Search
+  */
   Map<UndirectedGraphNode, UndirectedGraphNode> vertexMap = new HashMap<>();
   Queue<UndirectedGraphNode> queue = new LinkedList<>();
 
+  /*
+    Add the start node to the queue.
+    Give the start node a clone in the vertexMap
+  */
   queue.add(start);
   vertexMap.put(start, new UndirectedGraphNode(start.label));
 
+  /*
+    The breadth first search continues until we have processed all vertices
+    in the original graph. We know this is done when the queue is empty
+  */
   while(!queue.isEmpty()) {
 
+    /*
+      We grab a node. We will express all of the edges coming off of this
+      node.
+    */
     UndirectedGraphNode currVertex = queue.remove();
 
+    /*
+      Iterate over all neighbors.
+    */
     for (UndirectedGraphNode neighbor : currVertex.neighbors) {
 
+      /*
+        Has this neighbor been given a clone?
+      */
       if (!vertexMap.containsKey(neighbor)) {
+        /*
+          No. Give it a mapping and add the original neighbor to the
+          search queue so we can express ITS edges later
+        */
         vertexMap.put(neighbor, new UndirectedGraphNode(neighbor.label));
         queue.add(neighbor);
       }
 
+      /*
+        Draw the edge from currVertex's clone to neighbor's clone.
+        Do you see how our hashtable makes this quick access possible?
+      */
       vertexMap.get(currVertex).neighbors.add(vertexMap.get(neighbor));
     }
   }
 
+  /*
+    Return the clone of the start. This is the entry point for the
+    cloned graph section.
+  */
   return vertexMap.get(start);
 }
