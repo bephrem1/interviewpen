c9287ee4d23b2ac0adca0b260259f4582261f6ad
Benyam Ephrem <ephremdsg@gmail.com>
2019-02-04 11:38:16 -0500

    I did half of this

--- Stacks & Queues/queueWith2Stacks.java
+++ Stacks & Queues/queueWith2Stacks.java
@@ -1,7 +1,7 @@
 /*
   Implement Queue using Stacks - LeetCode: https://leetcode.com/problems/implement-queue-using-stacks/
 
-  Authorship: ALL credit for the code in this file goes to the authors of the
+  Authorship: Credit for the code in this file goes to the authors of the
   book "Elements of Programming Interviews" by Adnan Aziz, Amit Prakash, and
   Tsung-Hsien Lee.
 
