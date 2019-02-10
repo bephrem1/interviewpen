b05b1ec7db41d2b3f81190d6cf7f3057789df0dc
Benyam Ephrem <31268101+bephrem1@users.noreply.github.com>
2019-02-10 08:45:31 -0500

    Update levelOrderTraversal.java

--- Trees, Binary Trees, & Binary Search Trees/levelOrderTraversal.java
+++ Trees, Binary Trees, & Binary Search Trees/levelOrderTraversal.java
@@ -6,7 +6,7 @@
   Runtime: 1 ms, faster than 82.85% of Java online submissions for Binary Tree Level Order Traversal.
   Memory Usage: 26.5 MB, less than 64.46% of Java online submissions for Binary Tree Level Order Traversal.
 
-  The video to explain this code is here: [a link will live here someday]
+  The video to explain this code is here: https://www.youtube.com/watch?v=gcR28Hc2TNQ
 */
 
 public class TreeNode {
