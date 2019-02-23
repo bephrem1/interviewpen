ca50f319f5e922251aa99e6e56e45e1f7f72d351
Benyam Ephrem <ephremdsg@gmail.com>
2019-02-23 09:29:33 -0500

    eval RPN

---
+++ Stacks & Queues/evaluateReversePolishNotation.java
@@ -0,0 +1,53 @@
+/*
+  Evaluate Reverse Polish Notation - LeetCode: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+  This code passes all Leetcode test cases as of Feb. 23 2019
+  Runtime: 5 ms, faster than 97.02% of Java online submissions for Evaluate Reverse Polish Notation.
+
+  The video to explain this code is here: [a link to a video will live here someday]
+*/
+
+/*
+  We assume we get a valid Reverse Polish Notation expression encoded
+  in the string array
+*/
+public int evalRPN(String[] tokens) {
+
+  /*
+    We will use these to hold the operands when we need to hold
+    them somewhere temporary for operations where we want to make
+    order obvious
+  */
+  int firstOperand;
+  int secondOperand;
+
+  Stack<Integer> stack = new Stack<Integer>();
+
+  for (String token : tokens) {
+    switch (token) {
+      case "+":
+        stack.push(stack.pop() + stack.pop());
+        break;
+      case "/":
+        secondOperand = stack.pop();
+        firstOperand = stack.pop();
+        stack.push(firstOperand / secondOperand);
+        break;
+      case "*":
+        stack.push(stack.pop() * stack.pop());
+        break;
+      case "-":
+        secondOperand = stack.pop();
+        firstOperand = stack.pop();
+        stack.push(firstOperand - secondOperand);
+        break;
+      default:
+        stack.push(Integer.parseInt(token)); // Must be a number if it isn't a "+", "-", "/", or "*"
+    }
+  }
+
+  /*
+    The result will be the 1 and only item on the stack
+  */
+  return stack.pop();
+}
