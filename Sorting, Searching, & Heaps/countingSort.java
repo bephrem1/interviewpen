b2223bc9fef9fba4b8193a31ce9a8a0fc4a7ef38
Benyam Ephrem <bephrem@wireless-10-104-20-96.umd.edu>
2019-04-11 14:22:04 -0400

    counting sort

---
+++ Sorting, Searching, & Heaps/countingSort.java
@@ -0,0 +1,81 @@
+/*
+  Counting Sort.
+
+  The video to explain this code is here: [a link will live here someday]
+*/
+
+public int[] countingSort(int[] arr, int k) {
+
+  /*
+    Our input is the 'arr' to sort and the total number of
+    unique number values in the array...'k'
+
+    'k' happens to be 1 + maxNumberInTheArray because
+    imagine the array [0, 3, 2, 4]. Our 'counts' array would need to
+    track occurrences for 0, 1, 2, 3, 4. This is 5 values.
+
+    The maxNumberInTheArray is 4 ... 4 + 1 = 5 ... k is 5. This is
+    because we include 0 to the range from 1 to the maxNumberInTheArray.
+
+    Key things to help your understanding:
+      - arr[i] -> A value in the original array
+      BEFORE AGGREGATION
+        - counts[arr[i]] -> The # of occurrences of the value arr[i]
+      AFTER AGGREGATION
+        - counts[arr[i]] -> The # of occurrences of values <= the value arr[i]
+                            (not including occurrences of 0)
+  */
+  int n = arr.length;
+  int sortedOutput[] = new int[n];
+
+  /*
+    The count array keeps track of the occurrences of the respective number
+    represented by index 'i'.
+
+    Index 0 has the number of occurrences for index 0.
+    Index 1 has the number of occurrences for index 1.
+    and so on...
+  */
+  int count[] = new int[k];
+  for (int i = 0; i <= k - 1; i++) {
+    count[i] = 0;
+  }
+
+  /*
+    Iterate through the input array and update the 'count' array tracking
+    occurrences
+  */
+  for (int i = 0; i < n; i++) {
+    count[arr[i]]++;
+  }
+
+  /*
+    Aggregate step. Reformat the counts array. Take running sums iteratively.
+    When we are done what each position count[i] will mean is the number of occurrences
+    of values <= to the value i (not including occurrences of 0). Here is the transformation:
+
+      index    0  1  2  3  4
+      count = [0, 3, 2, 1, 2]
+
+      index    0  1  2  3  4
+      count = [0, 3, 5, 6, 8]
+
+    So for example count[2]..there are 5 occurrences of #'s in 'arr' that are <= 2. And
+    we  see that more specifically there are 3 occurrences of 1 and 2 occurrences of 2 so
+    this is true.
+  */
+  for (int i = 1; i <= k - 1; i++) {
+    count[i] += count[i - 1];
+  }
+
+  /*
+    Place items into the output array processing items from the ORIGINAL array back to front.
+    Watch the explanation video, the placement can be confusing.
+  */
+  for (int i = n - 1; i >= 0; i--) {
+    sortedOutput[ count[arr[i]] - 1 ] = arr[i];  // Place an item
+    count[arr[i]]--;                             // Adjust the occurrence count for each # that we place into the final array.
+  }
+
+  return sortedOutput;
+}
