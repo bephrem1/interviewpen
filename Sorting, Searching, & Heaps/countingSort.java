d9d48d617a9af74fd9c397afbfd7b2c55f8c81b7
Benyam Ephrem <ephremdsg@gmail.com>
2019-04-11 22:10:39 -0400

    counting sort

--- Sorting, Searching, & Heaps/countingSort.java
+++ Sorting, Searching, & Heaps/countingSort.java
@@ -23,7 +23,6 @@ public int[] countingSort(int[] arr, int k) {
         - counts[arr[i]] -> The # of occurrences of the value arr[i]
       AFTER AGGREGATION
         - counts[arr[i]] -> The # of occurrences of values <= the value arr[i]
-                            (not including occurrences of 0)
   */
   int n = arr.length;
   int sortedOutput[] = new int[n];
@@ -36,36 +35,36 @@ public int[] countingSort(int[] arr, int k) {
     Index 1 has the number of occurrences for index 1.
     and so on...
   */
-  int count[] = new int[k];
+  int counts[] = new int[k];
   for (int i = 0; i <= k - 1; i++) {
-    count[i] = 0;
+    counts[i] = 0;
   }
 
   /*
-    Iterate through the input array and update the 'count' array tracking
+    Iterate through the input array and update the 'counts' array tracking
     occurrences
   */
   for (int i = 0; i < n; i++) {
-    count[arr[i]]++;
+    counts[arr[i]]++;
   }
 
   /*
     Aggregate step. Reformat the counts array. Take running sums iteratively.
-    When we are done what each position count[i] will mean is the number of occurrences
-    of values <= to the value i (not including occurrences of 0). Here is the transformation:
+    When we are done what each position counts[i] will mean is the number of occurrences
+    of values <= to the value i. Here is the transformation:
 
-      index    0  1  2  3  4
-      count = [0, 3, 2, 1, 2]
+      index     0  1  2  3  4
+      counts = [0, 3, 2, 1, 2]
 
-      index    0  1  2  3  4
-      count = [0, 3, 5, 6, 8]
+      index     0  1  2  3  4
+      counts = [0, 3, 5, 6, 8]
 
-    So for example count[2]..there are 5 occurrences of #'s in 'arr' that are <= 2. And
+    So for example counts[2]..there are 5 occurrences of #'s in 'arr' that are <= 2. And
     we  see that more specifically there are 3 occurrences of 1 and 2 occurrences of 2 so
     this is true.
   */
   for (int i = 1; i <= k - 1; i++) {
-    count[i] += count[i - 1];
+    counts[i] += counts[i - 1];
   }
 
   /*
@@ -73,8 +72,8 @@ public int[] countingSort(int[] arr, int k) {
     Watch the explanation video, the placement can be confusing.
   */
   for (int i = n - 1; i >= 0; i--) {
-    sortedOutput[ count[arr[i]] - 1 ] = arr[i];  // Place an item
-    count[arr[i]]--;                             // Adjust the occurrence count for each # that we place into the final array.
+    sortedOutput[ counts[arr[i]] - 1 ] = arr[i];  // Place an item
+    counts[arr[i]]--;                             // Adjust the occurrence count for each # that we place into the final array.
   }
 
   return sortedOutput;
