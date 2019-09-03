fc10194ad1df860ba12aaf74720e26657484a48b
Benyam Ephrem <ephremdsg@gmail.com>
2019-09-03 17:47:50 -0400

    Permutations

---
+++ Dynamic Programming, Recursion, & Backtracking/permutations.java
@@ -0,0 +1,46 @@
+/*
+  Permutations - LeetCode: https://leetcode.com/problems/permutations
+
+  This code passes all Leetcode test cases as of Sept. 3th 2019
+  Runtime: 1 ms, faster than 97.78% of Java online submissions for Permutations.
+*/
+
+public List<List<Integer>> permute(int[] originalArray) {
+  List<List<Integer>> allPermutations = new ArrayList<>();
+  generateAllPermutations(new ArrayList<>(), originalArray, allPermutations);
+  return allPermutations;
+}
+
+private void generateAllPermutations(
+  List<Integer> runningChoices,
+  int[] originalArray,
+  List<List<Integer>> allPermutations
+) {
+  if (runningChoices.size() == originalArray.length) {
+    allPermutations.add(new ArrayList<>(runningChoices));
+    return;
+  }
+
+  /*
+    Every stack frame of this function call represents the expression of trying (almost) all items in every "slot" in the array.
+    The recursion stops when we are choosing on 1 past the final "slot".
+  */
+  for (int i = 0; i < originalArray.length; i++) {
+    int choice = originalArray[i];
+
+    // Skip if element already exists in 'runningChoices'
+    if (runningChoices.contains(choice)) {
+      continue;
+    }
+
+    // 1.) Choose - Add the item to the 'runningChoices'
+    runningChoices.add(choice);
+
+    // 2.) Explore - Recurse on the choice
+    generateAllPermutations(runningChoices, originalArray, allPermutations);
+
+    // 3.) Unchoose - We have returned from the recursion, remove the choice we made.
+    // The next iteration will try another item in the "slot" we are working on.
+    runningChoices.remove(runningChoices.size() - 1);
+  }
+}
