8fc6f28bd79b91eb425acc0b6ec0dc85ed28e43e
Benyam Ephrem <ephremdsg@gmail.com>
2019-09-03 17:29:22 -0400

    Clarify n queens

--- Dynamic Programming, Recursion, & Backtracking/nQueens.java
+++ Dynamic Programming, Recursion, & Backtracking/nQueens.java
@@ -1,81 +1,65 @@
 /*
-  Authorship: Credit for the code in this file goes to the authors of the
-  book "Elements of Programming Interviews" by Adnan Aziz, Amit Prakash, and
-  Tsung-Hsien Lee.
-  
-  I have just adapted the solution to pass on Leetcode, added explanatory
-  comments, reformatted the code, & changed variable names for understanding.
-
   N-Queens - LeetCode: https://leetcode.com/problems/n-queens/
 
-  This code passes all Leetcode test cases as of Feb. 2 2019
-  Runtime*: 7 ms, faster than 41.80% of Java online submissions for N-Queens.
-  Memory Usage: 27.7 MB, less than 70.21% of Java online submissions for N-Queens.
-
-  *Using a char array will be faster than using a Strings
-
-  The video to explain this code is here: https://www.youtube.com/watch?v=wGbuCyNpxIg
+  This code passes all Leetcode test cases as of Sept. 1 2019
+  Runtime: 5 ms, faster than 38.11% of Java online submissions for N-Queens.
 */
 
 public List<List<String>> solveNQueens(int n) {
-  List<List<String>> result = new ArrayList<>();
-  solveNQueens(n, 0, new ArrayList<Integer>(), result);
-  return result;
+  List<List<String>> results = new ArrayList<>();
+  solveNQueens(0, n, new ArrayList<>(), results);
+  return results;
 }
 
-private void solveNQueens(int n, int row, List<Integer> colPlacements,
-                                  List<List<String>> result){
+private void solveNQueens(int row, int n, List<Integer> colPlacements, List<List<String>> results) {
   if (row == n) {
-
     /*
       All n queens have been placed in the n rows. We have
       reached the bottom of our recursion. We can now add
-      the colPlacements to the result.
+      the colPlacements to the results.
     */
-    result.add(generateBoardFromPlacements(colPlacements, n));
+    results.add(generateBoardFromPlacements(colPlacements, n));
+    return;
+  }
 
-  } else {
+  /*
+    Try all columns in the current row that we are making
+    a choice on.
+
+    The colPlacements list will hold the column we place a
+    queen for the i'th row.
+
+    So if I have [ 1, 3, 0, 2 ] that means:
+
+    It is a 4 x 4 board.
+
+    row index 0 has a queen placed in column index 1
+    row index 1 has a queen placed in column index 3
+    row index 2 has a queen placed in column index 0
+    row index 3 has a queen placed in column index 2
+  */
+  for (int col = 0; col < n; col++) {
 
     /*
-      Try ALL columns in the current row that we are making
-      a choice on.
-
-      The colPlacements list will hold the column we place a
-      queen for the i'th row.
-
-      So if I have [ 1, 3, 0, 2 ] that means:
-
-      It is a 4 x 4 board.
-
-      row index 0 has a queen placed in column index 1
-      row index 1 has a queen placed in column index 3
-      row index 2 has a queen placed in column index 0
-      row index 3 has a queen placed in column index 2
+      Record a column placement for this row
     */
-    for (int col = 0; col < n; col++) {
+    colPlacements.add(col);
 
-      /*
-        Record a column placement for this row
-      */
-      colPlacements.add(col);
-
-      /*
-        If it is a valid placement we recurse to work on
-        the next row (row + 1) in a recursive call
-      */
-      if (isValid(colPlacements)) {
-        solveNQueens(n, row + 1, colPlacements, result);
-      }
-
-      /*
-        We are done exploring with that placement and now we
-        will remove it from our colPlacements. We will loop
-        back around and try more column placements for this
-        row (if there are any left)
-      */
-      colPlacements.remove(colPlacements.size() - 1);
+    /*
+      If it is a valid placement we recurse to work on
+      the next row (row + 1) in a recursive call
+    */
+    if (isValid(colPlacements)) {
+      solveNQueens(row + 1, n, colPlacements, results);
     }
 
+    /*
+      We are done exploring with that placement and now we
+      will remove it from our colPlacements. We will loop
+      back around and try more column placements for this
+      row (if there are any left)
+    */
+    colPlacements.remove(colPlacements.size() - 1);
   }
 }
 
@@ -100,23 +84,20 @@ private static boolean isValid(List<Integer> colPlacements) {
     /*
       Get the absolute difference between:
 
-      1.) The column of the already placed queen we are comparing against:
-        colPlacements.get(i)
+      1.) The column of the already placed queen we are comparing against -> colPlacements.get(ithQueenRow)
 
-      2.) The column of the queen we just placed 
-        colPlacements.get(rowWeAreValidatingOn)
+      2.) The column of the queen we just placed -> colPlacements.get(rowWeAreValidatingOn)
     */
-    int absoluteColumnDistance = Math.abs(colPlacements.get(ithQueenRow) -
-                                  colPlacements.get(rowWeAreValidatingOn));
+    int absoluteColumnDistance = Math.abs(colPlacements.get(ithQueenRow) - colPlacements.get(rowWeAreValidatingOn));
 
     /*
-      1.) If the absolute difference in columns is 0 then we placed in a column being
-      attacked by the i'th queen.
-        absoluteColumnDistance == 0
+      1.) absoluteColumnDistance == 0
+        If the absolute difference in columns is 0 then we placed in a column being
+        attacked by the i'th queen.
 
-      2.) If the absolute difference in columns equals the distance in rows from the
-      i'th queen we placed then the queen we just placed is attacked diagonally.
-        absoluteColumnDistance == rowWeAreValidatingOn - i
+      2.) absoluteColumnDistance == rowDistance
+        If the absolute difference in columns equals the distance in rows from the
+        i'th queen we placed, then the queen we just placed is attacked diagonally.
 
       For Constraint #2 imagine this:
 
@@ -127,13 +108,16 @@ private static boolean isValid(List<Integer> colPlacements) {
         ". . . ."
       ]
 
-      Absolute Column Distance Between Queen 2 & 3 == 1.
-      Queen 2 is in col 0, Queen 3 is in col 1. 1 - 0 = 1.
+      1.) 
+        Absolute Column Distance Between Queen 2 & 3 == 1.
+        Queen 2 is in col 0, Queen 3 is in col 1. 1 - 0 = 1.
 
-      Absolute Row Distance Between Queen 2 & 3 == 1
-      Queen 2 is in row 1, Queen 3 is in row 2. 2 - 1 = 1.
+      2.)
+        Absolute Row Distance Between Queen 2 & 3 == 1
+        Queen 2 is in row 1, Queen 3 is in row 2. 2 - 1 = 1.
     */
-    if (absoluteColumnDistance == 0 || absoluteColumnDistance == rowWeAreValidatingOn - ithQueenRow) {
+    int rowDistance = rowWeAreValidatingOn - ithQueenRow;
+    if (absoluteColumnDistance == 0 || absoluteColumnDistance == rowDistance) {
       return false;
     }
   }
@@ -181,7 +165,6 @@ private List<String> generateBoardFromPlacements(List<Integer> colPlacements, in
       Add the row to the board
     */
     board.add(sb.toString());
-
   }
 
   return board;
