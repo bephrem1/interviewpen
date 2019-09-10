586429258633599a04614b1fe2e3098c4c5068af
Benyam Ephrem <ephremdsg@gmail.com>
2019-09-10 15:00:41 -0400

    more linked list stuff

--- Linked Lists/linkedListFundamentals.java
+++ Linked Lists/linkedListFundamentals.java
@@ -30,6 +30,13 @@ public class LinkedListFundamentals {
     System.out.println("printListInReverseIterative");
     printListInReverseIterative(head);
     newline();
+
+    System.out.println("deepCopyListIterative");
+    ListNode newListHead = deepCopyListIterative(head);
+    printListIterative(newListHead);
+    System.out.println("New list head value: " + newListHead.value);
+    System.out.println("Are the 2 list head's equivalent in memory?: " + (a == newListHead));
+    newline();
   }
 
   private static void printListIterative(ListNode head) {
@@ -102,6 +109,50 @@ public class LinkedListFundamentals {
 
   private static void newline() {
     System.out.println();
+  }
+
+  private static ListNode deepCopyListIterative(ListNode head) {
+    /*
+      A "dummy head" node allows us to not have to handle the
+      initial empty case of a new list not being initialized
+      with a first node.
+
+      We can immediately begin appending to this new list and
+      "chop off" this node at the end and it will be garbage
+      collected (probably, if no one is using the reference later).
+    */
+    ListNode newListHead = new ListNode(-1);
+
+    /*
+      Point a working pointer to the old list, point a working pointer
+      to the dummy head.
+
+      We will build off the 'next' field of the dummy head immediately.
+    */
+    ListNode oldListWorkingPointer = head;
+    ListNode newListWorkingPointer = newListHead;
+
+    while (oldListWorkingPointer != null) {
+      ListNode newNode = new ListNode(oldListWorkingPointer.value);
+
+      // Append the deep copied node to the new list we are building
+      newListWorkingPointer.next = newNode;
+
+      // Advance to the next node in the old list to process
+      oldListWorkingPointer = oldListWorkingPointer.next;
+
+      // Move to the new node we just appended
+      newListWorkingPointer = newListWorkingPointer.next;
+
+      /*
+        We can also do the following, same reference to the same
+        node (the newly appended node):
+      */
+      // newListWorkingPointer = newNode;
+    }
+
+    // "Cut off" the dummy head just leaving the pure new list created
+    return newListHead.next;
   }
 
   /*
