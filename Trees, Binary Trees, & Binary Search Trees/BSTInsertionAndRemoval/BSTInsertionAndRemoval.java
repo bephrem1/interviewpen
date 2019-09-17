8f08f49217e8901a6bf0c0aa7ab4d941d456cc0c
Benyam Ephrem <ephremdsg@gmail.com>
2019-09-17 09:18:31 -0400

    bst removal and insertion

---
+++ Trees, Binary Trees, & Binary Search Trees/BSTInsertionAndRemoval/BSTInsertionAndRemoval.java
@@ -0,0 +1,84 @@
+/*
+  Insert into a Binary Search Tree - LeetCode: https://leetcode.com/problems/insert-into-a-binary-search-tree/
+  Delete Node In A BST: https://leetcode.com/problems/delete-node-in-a-bst/
+
+  This code passes all Leetcode test cases as of Sept. 17 2019
+*/
+
+class InsertIntoABST {
+  public TreeNode insertIntoBST(TreeNode root, int val) {
+    if (root == null) {
+      return new TreeNode(val);
+    }
+
+    if (root.val > val) {
+      root.left = insertIntoBST(root.left, val);
+    } else {
+      root.right = insertIntoBST(root.right, val);
+    }
+
+    return root;
+  }
+}
+
+class DeleteFromABST {
+  public TreeNode deleteNode(TreeNode root, int key) {
+    if (root == null) {
+      return null;
+    }
+
+    if (key < root.val) {
+      root.left = deleteNode(root.left, key);
+    } else if (key > root.val) {
+      root.right = deleteNode(root.right, key);
+    } else {
+      /*
+        We have found the node that needs to be removed,
+        we have 3 cases:
+
+        1.) Empty left subtree: We can just return the
+        right subtree of the node, cutting root itself out
+
+        2.) Empty right subtree: We can just return the
+        left subtree of the node, cutting root itself out
+
+        3.) Non-empty left and right subtrees: We want the
+        next node in the inorder traversal after this node
+        to take this node's place.
+
+        Hop to the right and go all the way left to get the
+        next item in the inorder traversal.
+      */
+      if (root.left == null) {
+        return root.right;
+      } else if (root.right == null) {
+        return root.left;
+      }
+
+      TreeNode nextInorderNode = getNextInorderNode(root.right);
+      root.val = nextInorderNode.val;
+
+      /*
+        Now update root's right subtree by removing the actual
+        'nextInorderNode' object from it sine we just placed
+        its value into root
+      */
+      root.right = deleteNode(root.right, nextInorderNode.val);
+    }
+
+    return root;
+  }
+
+  private TreeNode getNextInorderNode(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+
+    TreeNode curr = root;
+    while (curr.left != null) {
+      curr = curr.left;
+    }
+
+    return curr;
+  }
+}
