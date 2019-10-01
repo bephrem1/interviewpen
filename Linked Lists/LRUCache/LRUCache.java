a51bf40827805865a6caf3bad95f4a1d79d8e7ad
Benyam Ephrem <ephremdsg@gmail.com>
2019-10-01 14:58:55 -0400

    lru cache update

--- Linked Lists/LRUCache/LRUCache.java
+++ Linked Lists/LRUCache/LRUCache.java
@@ -1,180 +1,123 @@
 /*
   LRU Cache - LeetCode: https://leetcode.com/problems/lru-cache/
-
-  An adaption of the answer from user "liaison" on Leetcode.
-  Link: https://leetcode.com/problems/lru-cache/discuss/45911/Java-Hashtable-%2B-Double-linked-list-(with-a-touch-of-pseudo-nodes)
-
-  Revision by Benyam Ephrem (Dec. 31th 2018)
-    > Making variable names more conventional
-    > Adding more clarifying comments
-    > Moving code around to be more conventional
-
-  This code passes all Leetcode test cases as of Dec. 31st 2018
-  Runtime: 77 ms, faster than 95.85% of Java online submissions for LRU Cache.
-
-  The video to explain this code is here: https://www.youtube.com/watch?v=S6IfqDXWa10
+  This code passes all Leetcode test cases as of Oct. 1st 2019
 */
+import java.util.*;
 
 class LRUCache {
+  // Hashtable backs up the Doubly Linked List for O(1) access to cache items
+  Map<Integer, ListNode> hashtable = new HashMap<Integer, ListNode>();
+  ListNode head;
+  ListNode tail;
 
-    /*
-      Our internal definition of a doubly linked list
-      node, put in this class for convenience since
-      this is submitted in one file on Leetcode
-    */
-    private class DNode {
-      int key;
-      int value;
-      DNode prev;
-      DNode next;
+  int totalItemsInCache;
+  int maxCapacity;
+
+  public LRUCache(int maxCapacity) {
+    // Cache starts empty and capacity is set by client
+    totalItemsInCache = 0;
+    this.maxCapacity = maxCapacity;
+
+    // Dummy head and tail nodes to avoid empty states
+    head = new ListNode();
+    tail = new ListNode();
+
+    // Wire the head and tail together
+    head.next = tail;
+    tail.prev = head;
+  }
+
+  public int get(int key) {
+    ListNode node = hashtable.get(key);
+
+    if (node == null) {
+      return -1; // we should throw an exception here, but for Leetcode's sake
     }
 
-    private Map<Integer, DNode> hashtable = new HashMap<Integer, DNode>();
-    private DNode head, tail;
-    private int totalItemsInCache;
-    private int maxCapacity;
+    // Item has been accessed. Move to the front of the cache
+    moveToHead(node);
 
-    public LRUCache(int maxCapacity) {
+    return node.value;
+  }
 
-      // Cache starts empty and capacity is set by client
-      totalItemsInCache = 0;
-      this.maxCapacity = maxCapacity;
+  public void put(int key, int value) {
+    ListNode node = hashtable.get(key);
 
-      // Initialize the dummy head of the cache
-      head = new DNode();
-      head.prev = null;
+    if (node == null) {
+      // Item not found, create a new entry
+      ListNode newNode = new ListNode();
+      newNode.key = key;
+      newNode.value = value;
 
-      // Init the dummy tail of the cache
-      tail = new DNode();
-      tail.next = null;
+      // Add to the hashtable and the actual list that represents the cache
+      hashtable.put(key, newNode);
+      addToFront(newNode);
+      totalItemsInCache++;
 
-      // Wire the head and tail together
-      head.next = tail;
-      tail.prev = head;
-    }
-
-    /*
-      Retrieve an item from the cache
-    */
-    public int get(int key) {
-
-      DNode node = hashtable.get(key);
-      boolean itemFoundInCache = node != null;
-
-      // Empty state check. Should raise exception here.
-      if(!itemFoundInCache){
-        return -1;
+      // If over capacity remove the LRU item
+      if (totalItemsInCache > maxCapacity) {
+        removeLRUEntry();
       }
-
-      // Item has been accessed. Move to the front of the list.
+    } else {
+      // If item is found in the cache, just update it and move it to the head of the list
+      node.value = value;
       moveToHead(node);
-
-      return node.value;
     }
+
+  }
+
+  private void removeLRUEntry() {
+    ListNode tail = popTail();
+
+    hashtable.remove(tail.key);
+    --totalItemsInCache;
+  }
+
+  private ListNode popTail() {
+    ListNode tailItem = tail.prev;
+    removeFromList(tailItem);
+
+    return tailItem;
+  }
+
+  private void addToFront(ListNode node) {
+    // Wire up the new node being to be inserted
+    node.prev = head;
+    node.next = head.next;
 
     /*
-      Add an item to the cache if it is not already there,
-      if it is already there update the value and move it
-      to the front of the cache
+      Re-wire the node after the head. Our node is still sitting "in the middle of nowhere".
+      We got the new node pointing to the right things, but we need to fix up the original
+      head & head's next.
+
+      head <-> head.next <-> head.next.next <-> head.next.next.next <-> ...
+      ^            ^
+      |- new node -|
+
+      That's where we are before these next 2 lines.
     */
-    public void put(int key, int value) {
+    head.next.prev = node;
+    head.next = node;
+  }
 
-      DNode node = hashtable.get(key);
-      boolean itemFoundInCache = node != null;
+  private void removeFromList(ListNode node) {
+    ListNode savedPrev = node.prev;
+    ListNode savedNext = node.next;
 
-      if(!itemFoundInCache){
+    savedPrev.next = savedNext;
+    savedNext.prev = savedPrev;
+  }
 
-        // Create a new node
-        DNode newNode = new DNode();
-        newNode.key = key;
-        newNode.value = value;
+  private void moveToHead(ListNode node) {
+    removeFromList(node);
+    addToFront(node);
+  }
 
-        // Add to the hashtable and the doubly linked list
-        hashtable.put(key, newNode);
-        addNode(newNode);
+  private class ListNode {
+    int key;
+    int value;
 
-        // We just added an item to the cache
-        totalItemsInCache++;
-
-        // If over capacity evict an item with LRU cache eviction policy
-        if(totalItemsInCache > maxCapacity){
-          removeLRUEntryFromStructure();
-        }
-
-      } else {
-        // If item is in cache just update and move it to the head
-        node.value = value;
-        moveToHead(node);
-      }
-
-    }
-
-    /*
-      Remove the least used entry from the doubly linked
-      list as well as the hashtable. Hence it is evicted
-      from the whole LRUCache structure
-    */
-    private void removeLRUEntryFromStructure() {
-      DNode tail = popTail();
-      hashtable.remove(tail.key);
-      --totalItemsInCache;
-    }
-
-    /*
-      Insertions to the doubly linked list will always
-      be right after the dummy head
-    */
-    private void addNode(DNode node){
-
-      // Wire the node being inserted
-      node.prev = head;
-      node.next = head.next;
-
-      // Re-wire the head's old next
-      head.next.prev = node;
-
-      // Re-wire the head to point to the inserted node
-      head.next = node;
-    }
-
-    /*
-      Remove the given node from the doubly linked list
-    */
-    private void removeNode(DNode node){
-
-      // Grab reference to the prev and next of the node
-      DNode savedPrev = node.prev;
-      DNode savedNext = node.next;
-
-      // Cut out going forwards
-      savedPrev.next = savedNext;
-
-      // Cut out going backards
-      savedNext.prev = savedPrev;
-    }
-
-    /*
-      Move a node to the head of the doubly linked lsit
-    */
-    private void moveToHead(DNode node){
-      removeNode(node);
-      addNode(node);
-    }
-
-    /*
-      Pop the last item from the structure
-    */
-    private DNode popTail(){
-      DNode itemBeingRemoved = tail.prev;
-      removeNode(itemBeingRemoved);
-      return itemBeingRemoved;
-    }
-
+    ListNode prev;
+    ListNode next;
+  }
 }
-
-/**
- * The LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(maxCapacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
