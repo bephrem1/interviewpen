c87252de1b2b9ece11b2bb2ecfe8b31be54bbead
Benyam Ephrem <ephremdsg@gmail.com>
2019-10-02 16:52:30 -0400

    nearest repeated entries

---
+++ Hashtables/NearestRepeatedEntries/NearestRepeatedEntries.java
@@ -0,0 +1,44 @@
+/*
+  Write a program that finds the distance between the closest pair
+  of equivalent entries
+*/
+
+import java.util.*;
+
+public class NearestRepeatedEntries {
+  public static void main(String args[]) {
+    String[] sentence = new String[]{
+      "This",
+      "is",
+      "a",
+      "sentence",
+      "with",
+      "is",
+      "repeated",
+      "then",
+      "repeated"
+    };
+
+    System.out.println("Distance between closest repeated entries: " + distanceOfClosestRepeatedEntries(sentence));
+  }
+
+  private static int distanceOfClosestRepeatedEntries(String[] sentence) {
+    Map<String, Integer> wordToIndexLastSeenAt = new HashMap<>();
+    int nearestRepeatedEntryDistance = Integer.MAX_VALUE;
+
+    for (int i = 0; i < sentence.length; i++) {
+      String word = sentence[i];
+
+      if (wordToIndexLastSeenAt.containsKey(word)) {
+        int lastAppearanceIndex = wordToIndexLastSeenAt.get(word);
+        int distanceToLastAppearance = i - lastAppearanceIndex;
+
+        nearestRepeatedEntryDistance = Math.min(nearestRepeatedEntryDistance, distanceToLastAppearance);
+      }
+
+      wordToIndexLastSeenAt.put(word, i);
+    }
+
+    return nearestRepeatedEntryDistance == Integer.MAX_VALUE ? -1 : nearestRepeatedEntryDistance;
+  }
+}
