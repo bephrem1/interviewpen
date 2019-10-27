76098402ef1b4cc236d27bb93d7b436832d8ff78
Benyam Ephrem <ephremdsg@gmail.com>
2019-10-27 12:34:10 -0400

    buy and sell once

--- Dynamic Programming, Recursion, & Backtracking/BuyAndSellOnce/BuyAndSellOnce.java
+++ Dynamic Programming, Recursion, & Backtracking/BuyAndSellOnce/BuyAndSellOnce.java
@@ -18,10 +18,16 @@ class LinearSpace {
       sell today - day i)
 
       2.) Don't lengthen opt[i - 1]: If we decide to sell today
-      (day i) we will lose money versus if we sold yesterday. So
-      to answer opt[i] we just decide to buy today (buy on day i)
+      (day i) we will lose money (go below $0 profit).
+
+      To answer opt[i] we just decide to buy today (buy on day i)
       and we are back to a total profit of 0 ending at day i as
-      we could not extend yesterday's profit without hurting it.
+      we could not extend yesterday's profit without losing money.
+
+      The Bigger Picture: We know that an optimal answer will end
+      on one of these days. If we answer opt[i] for all days, we
+      know we will capture the optimal solution when the algorithm
+      is run.
     */
     int[] opt = new int[prices.length];
     opt[0] = 0;
