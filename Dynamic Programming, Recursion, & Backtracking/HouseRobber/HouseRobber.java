d063305d9e5fdb53c2810a6735ea04cdb6bae3df
Benyam Ephrem <ephremdsg@gmail.com>
2019-11-07 10:33:15 -0500

    Min interval overlap erase.

---
+++ Dynamic Programming, Recursion, & Backtracking/HouseRobber/HouseRobber.java
@@ -0,0 +1,8 @@
+/*
+  House Robber - LeetCode: https://leetcode.com/problems/house-robber/
+  This code passes all Leetcode test cases as of Oct. 30th 2019
+*/
+
+public int rob(int[] nums) {
+  
+}
---
+++ Greedy Algorithms/EraseIntervalOverlaps/EraseIntervalOverlaps.java
@@ -0,0 +1,36 @@
+/*
+  Non-overlapping Intervals - LeetCode: https://leetcode.com/problems/non-overlapping-intervals/
+  This code passes all Leetcode test cases as of Nov. 7 2019
+*/
+
+public int eraseOverlapIntervals(int[][] intervals) {
+  if (intervals.length == 0) {
+    return 0;
+  }
+
+  Arrays.sort(intervals, new TwoDimArrayComparator());
+
+  int endOfActiveInterval = intervals[0][1];
+  int totalIntervalsInNonoverlappingSet = 1;
+
+  for (int i = 1; i < intervals.length; i++) {
+    int[] interval = intervals[i];
+
+    int intervalStart = interval[0];
+    int intervalEnd = interval[1];
+
+    if (intervalStart >= endOfActiveInterval) {
+      endOfActiveInterval = intervalEnd;
+      totalIntervalsInNonoverlappingSet++;
+    }
+  }
+
+  return intervals.length - totalIntervalsInNonoverlappingSet;
+}
+
+// Sorts intervals in increasing order by end time
+class TwoDimArrayComparator implements Comparator<int[]> {
+  public int compare(int[] a, int[] b) {
+    return a[1] - b[1];
+  }
+}
