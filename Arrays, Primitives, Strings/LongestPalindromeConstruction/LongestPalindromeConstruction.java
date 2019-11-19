26af7e7b1e49f8458b219d67407e7271f555c8dc
Benyam Ephrem <ephremdsg@gmail.com>
2019-11-19 12:04:09 -0500

    Longest Palindrome Construction

---
+++ Arrays, Primitives, Strings/LongestPalindromeConstruction/LongestPalindromeConstruction.java
@@ -0,0 +1,83 @@
+/*
+  Longest Palindrome - LeetCode: https://leetcode.com/problems/longest-palindrome/
+  This code passes all Leetcode test cases as of Nov. 19 2019
+*/
+
+class WithHashtable {
+  public int longestPalindrome(String s) {
+    int longestPalindromeLength = 0;
+
+    Map<Character, Integer> characterToCount = new HashMap<>();
+    for (char c: s.toCharArray()) {
+      if (!characterToCount.containsKey(c)) {
+        characterToCount.put(c, 1);
+      } else {
+        characterToCount.put(c, characterToCount.get(c) + 1);
+      }
+    }
+
+    Character mostFrequentOddCharacter = null;
+    int mostFrequentOddCharacterFrequency = 0;
+    for (Map.Entry<Character, Integer> entry: characterToCount.entrySet()) {
+      char c = entry.getKey();
+      int count = entry.getValue();
+
+      if (count % 2 == 0) {
+        longestPalindromeLength += count; // character can mirror itself
+      } else {
+        if (count > mostFrequentOddCharacterFrequency) {
+          mostFrequentOddCharacter = c;
+          mostFrequentOddCharacterFrequency = count;
+        }
+      }
+    }
+
+    /*
+      Only place odd frequency character can happen is the middle, so
+      we "place" the most frequent odd character in the middle
+    */
+    longestPalindromeLength += mostFrequentOddCharacterFrequency;
+
+    /*
+      Now we remove 1 from each of the rest of the odd characters to turn
+      their frequency even
+    */
+    if (mostFrequentOddCharacter != null) {
+      characterToCount.remove(mostFrequentOddCharacter);
+
+      for (Map.Entry<Character, Integer> entry: characterToCount.entrySet()) {
+        char c = entry.getKey();
+        int count = entry.getValue();
+
+        if (count % 2 != 0) {
+          longestPalindromeLength += count - 1;
+        }
+      }
+    }
+
+    return longestPalindromeLength;
+  }
+}
+
+class WithSet {
+  public int longestPalindrome(String s) {
+    int matchings = 0;
+    Set<Character> unmatchedCharacters = new HashSet<Character>();
+
+    for (char c: s.toCharArray()) {
+      if (unmatchedCharacters.contains(c)) {
+        unmatchedCharacters.remove(c);
+        matchings++;
+      } else {
+        unmatchedCharacters.add(c);
+      }
+    }
+
+    int longestPalindromeLength = matchings * 2;
+    if (!unmatchedCharacters.isEmpty()) {
+      longestPalindromeLength++; // place 1 unmatched odd character in the middle
+    }
+
+    return longestPalindromeLength;
+  }
+}
---
+++ Arrays, Primitives, Strings/PatternSearchAndReplacement/PatternSearchAndReplacement.java
@@ -0,0 +1,8 @@
+/*
+  Find and Replace Pattern - LeetCode: https://leetcode.com/problems/find-and-replace-pattern/
+  This code passes all Leetcode test cases as of Nov. 19 2019
+*/
+
+public List<String> findAndReplacePattern(String[] words, String pattern) {
+  
+}
