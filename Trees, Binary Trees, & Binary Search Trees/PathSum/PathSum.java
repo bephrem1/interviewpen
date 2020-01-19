0579c6b8471fb21e1be8d8dd69d5c96164768895
Benyam Ephrem <ephremdsg@gmail.com>
2020-01-19 08:12:57 -0500

    Update PathSum.java

--- Trees, Binary Trees, & Binary Search Trees/PathSum/PathSum.java
+++ Trees, Binary Trees, & Binary Search Trees/PathSum/PathSum.java
@@ -88,52 +88,8 @@ class PathSumIIILinear {
       return 0;
     }
 
-    /*
-     * The key is that at each node we will know the root to node sum following the
-     * path we recursed on.
-     * 
-     * We will either overshoot, or undershoot, the target sum. We track prefixes to
-     * see if we can "compensate" by removing that prefix from the root to node path
-     * to hit the target sum.
-     * 
-     * 'prefixSumToTotalPrefixes' is a hashtable that maps: prefixSum -> total
-     * prefixes with that sum
-     * 
-     * But what is the context? The context is that the prefixes we track are all
-     * connected & previous to the node we will be sitting at.
-     * 
-     * So if we have: 10 / \ 5 -3 / \ \ 3 2 11 / \ \ 3 -2 1
-     * 
-     * And we are sitting at -2 in the last level, our prefix table will look like
-     * this:
-     * 
-     * { 0 -> 1 path: nothing - default 16 -> 1 path: 10 (root) -> 5 -> 3 -> -2 18
-     * -> 1 path: 10 (root) -> 5 -> 3 10 -> 1 path: 10 (root) 15 -> 1 path: 10
-     * (root) -> 5 }
-     * 
-     * Imagine our target is 8.
-     * 
-     * The breakdown: - Our root (10) to node (-2) path sum is 10 + 5 + 3 + -2 = 16
-     * - How far away are we from the target? - We do rootToNodeSum - target = (16)
-     * - (8) = 8 - This means we need to "chop off" 8 in value from this path
-     * 
-     * This is where the prefixes come in. We can look up in the hashtable, how many
-     * unique prefixes up to this node (starting at the root) had sum 8?
-     * 
-     * If they had sum 8 we can remove those to yield a "pruned path" that does not
-     * start from the root...but hits our target because it is connected to the node
-     * we are sitting at.
-     */
     Map<Integer, Integer> prefixSumToTotalPrefixes = new HashMap<>();
 
-    /*
-     * If we need to take away 0 from the root to leaf path to the node we sit at,
-     * then that is a complete path.
-     * 
-     * Namely, we have a current sum that requires no "compensation" with prefix
-     * reduction, we have hit the target sum. This will be a literal path from the
-     * actual root to the node that we will stand at.
-     */
     prefixSumToTotalPrefixes.put(0, 1);
 
     return findPathSum(root, 0, sum, prefixSumToTotalPrefixes);
@@ -145,55 +101,18 @@ class PathSumIIILinear {
       return 0;
     }
 
-    // Add the current node's value to the path we are tracking from the root to
-    // this node
     rootToNodeSum += node.val;
-
-    /*
-     * Based on sum of the path from the root to here, how much must we "truncate"
-     * our current path's sum (by removing a prefix also coming from the root) to
-     * create a new path with the target sum?
-     */
+    
     int amountToCompensateFor = rootToNodeSum - target;
     int totalPathsEndingAtThisNode = prefixSumToTotalPrefixes.getOrDefault(amountToCompensateFor, 0);
-
-    // Update the 'prefixSumToTotalPrefixes' with this root to node path itself
+    
     int totalPathsWithThisPathsSum = prefixSumToTotalPrefixes.getOrDefault(rootToNodeSum, 0);
     prefixSumToTotalPrefixes.put(rootToNodeSum, totalPathsWithThisPathsSum + 1);
 
-    // Investigate the total paths that can be completed in this node's left & right
-    // subtrees
     int totalCompletedPathsInThisSubtree = totalPathsEndingAtThisNode
         + findPathSum(node.left, rootToNodeSum, target, prefixSumToTotalPrefixes)
         + findPathSum(node.right, rootToNodeSum, target, prefixSumToTotalPrefixes);
 
-    /*
-     * Cleanup, when we are back, we need to remove this path (the root to this node
-     * path we are on) from the prefix table before we return to our caller:
-     * 
-     * So if we have: 10 / \ 5 -3 / \ \ 3 2 11 / \ \ 3 -2 1
-     * 
-     * And we are sitting at 3 in the last level, our prefix table will look like
-     * this:
-     * 
-     * { 0 -> 1 path: nothing - default 18 -> 1 path: 10 (root) -> 5 -> 3 21 -> 1
-     * path: 10 (root) -> 5 -> 3 -> 3 *** will get decreased by 1 since we will
-     * backtrack *** 10 -> 1 path: 10 (root) 15 -> 1 path: 10 (root) -> 5 }
-     * 
-     * Before we return to the 3 that called us (in the 2nd to last layer), we must
-     * remove our prefix entry from the hastable (which is notated as a path with
-     * sum 21...10 (root) -> 5 -> 3 -> 3)
-     * 
-     * After: { 0 -> 1 path: nothing - default 18 -> 1 path: 10 (root) -> 5 -> 3 21
-     * -> 0 *** can be removed if value hits 0 but doesn't matter *** 10 -> 1 path:
-     * 10 (root) 15 -> 1 path: 10 (root) -> 5 }
-     * 
-     * Then control will go back to the 3 (in the second to last level): 10 / \ 5 -3
-     * / \ \ >3< 2 11 / \ \ 3 -2 1
-     * 
-     * Real prefixes: { 0 -> 1 path: nothing - default 18 -> 1 path: 10 (root) -> 5
-     * -> 3 10 -> 1 path: 10 (root) 15 -> 1 path: 10 (root) -> 5 }
-     */
     prefixSumToTotalPrefixes.put(rootToNodeSum, prefixSumToTotalPrefixes.getOrDefault(rootToNodeSum, 0) - 1);
 
     return totalCompletedPathsInThisSubtree;
